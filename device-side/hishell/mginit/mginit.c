bdef51415ca9b287a4e9307383cca2e3c76233e0
gengyue <gengyue@minigui.org>
2020-09-02 16:38:18 +0800

    add mginit

---
+++ device-side/hishell/mginit/mginit.c
@@ -0,0 +1,256 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//                          IMPORTANT NOTICE
+//
+// The following open source license statement does not apply to any
+// entity in the Exception List published by FMSoft.
+//
+// For more information, please visit:
+//
+// https://www.fmsoft.cn/exception-list
+//
+//////////////////////////////////////////////////////////////////////////////
+/*
+** $Id: mginit.c 558 2008-03-04 09:02:19Z wangjian $
+**
+** The mginit program of MiniGUI Demostration Envirionment.
+**
+** Copyright (C) 2003 ~ 2017 FMSoft (http://www.fmsoft.cn).
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <minigui/common.h>
+#include <minigui/minigui.h>
+#include <minigui/gdi.h>
+#include <minigui/window.h>
+#include <minigui/control.h>
+#include <minigui/fixedmath.h>
+
+#ifdef _MGRM_THREADS
+    #error "*******************************************************************"
+    #error "**** Your MiniGUI is configured as MiniGUI-Threads.            ****"
+    #error "**** This program is a server of MiniGUI-Processes,            ****"
+    #error "****   it can only run on MiniGUI-Processes,                   ****"
+    #error "****   not on MiniGUI-Threads.                                 ****"
+    #error "**** If you want to configure MiniGUI as MiniGUI-Processes,    ****"
+    #error "****   please run `./configure --enable-procs'                 ****"
+    #error "****   when configuring MiniGUI                                ****"
+    #error "****   and build and reinstall MiniGUI.                        ****"
+    #error "**** Note that this is not a fatal error.                      ****"
+    #error "*******************************************************************"
+#else
+
+#include "statusbar.h"
+
+static HWND hStatusBar;
+static HWND hDockerBar;
+
+static const char* new_del_client_info [] =
+{
+    NULL,
+    "New comming in client: %s\n",
+    "Disconnecting client: %s\n"
+};
+
+static void on_new_del_client (int op, int cli)
+{
+    static int nr_clients = 0;
+    MG_Client* client = mgClients + cli;
+
+    if (op > 0 && op <= LCO_DEL_CLIENT)
+        printf (new_del_client_info [op], client?client->name:"NULL");
+
+    if (op == LCO_NEW_CLIENT) {
+        nr_clients ++;
+    }
+    else if (op == LCO_DEL_CLIENT) {
+        nr_clients --;
+        if (nr_clients == 0) {
+#if 0
+            SendMessage (hStatusBar, MSG_CLOSE, 0, 0);
+#else
+            printf ("There is no any client.\n");
+#endif
+        }
+        else if (nr_clients < 0) {
+            printf ("Serious error: nr_clients less than zero.\n");
+        }
+    }
+    else
+        printf ("Serious error: incorrect operations.\n");
+}
+
+static const char* change_layer_info [] =
+{
+        NULL,
+        "New layer created: %s; client: %s\n",
+        "Layer deleted: %s; client: %s\n",
+        "New client joined layer: (%s, %s)\n",
+        "Remove client from layer: (%s, %s)\n",
+        "Change topmost layer to %s; client: %s\n",
+        "Change active client: (%s, %s)\n",
+};
+
+static void adjust_boxes (int width, MG_Layer* layer)
+{
+    int left = _LEFT_BOXES;
+    MG_Layer* cur_layer = mgLayers;
+
+    while (cur_layer) {
+        if (cur_layer != layer) {
+            MoveWindow ((HWND)cur_layer->dwAddData, left, _MARGIN,
+                            width, _HEIGHT_CTRL, TRUE);
+            left += width;
+        }
+
+        cur_layer = cur_layer->next;
+    }
+}
+
+static void on_change_topmost (MG_Layer* layer)
+{
+    MG_Layer* cur_layer = mgLayers;
+
+    if (layer)
+        SendMessage ((HWND)layer->dwAddData, BM_SETCHECK, BST_CHECKED, 0);
+
+    while (cur_layer) {
+        if (cur_layer != layer) {
+            SendMessage ((HWND)cur_layer->dwAddData, BM_SETCHECK, 0, 0);
+        }
+
+        cur_layer = cur_layer->next;
+    }
+}
+
+static void on_change_layer (int op, MG_Layer* layer, MG_Client* client)
+{
+    static int nr_boxes = 0;
+    static int box_width = _MAX_WIDTH_LAYER_BOX;
+    int new_width;
+
+    if (op > 0 && op <= LCO_ACTIVE_CHANGED)
+        printf (change_layer_info [op], layer?layer->name:"NULL", 
+                        client?client->name:"NULL");
+
+    switch (op) {
+    case LCO_NEW_LAYER:
+        nr_boxes ++;
+        if (box_width * nr_boxes > _WIDTH_BOXES) {
+            new_width = _WIDTH_BOXES / nr_boxes;
+            if (new_width < _MIN_WIDTH_LAYER_BOX) {
+                new_width = _MIN_WIDTH_LAYER_BOX;
+            }
+
+            if (new_width != box_width) {
+                adjust_boxes (new_width, layer);
+                box_width = new_width;
+            }
+        }
+
+        layer->dwAddData = (DWORD)CreateWindow (CTRL_BUTTON, layer->name,
+                 WS_CHILD | WS_VISIBLE | BS_CHECKBOX | BS_PUSHLIKE | BS_CENTER,
+                 _ID_LAYER_BOX,
+                 _LEFT_BOXES + box_width * (nr_boxes - 1), _MARGIN,
+                 box_width, _HEIGHT_CTRL, hStatusBar, (DWORD)layer);
+        break;
+
+    case LCO_DEL_LAYER:
+        DestroyWindow ((HWND)(layer->dwAddData));
+        layer->dwAddData = 0;
+        nr_boxes --; 
+        if (box_width * nr_boxes < _WIDTH_BOXES) {
+            if (nr_boxes != 0)
+                new_width = _WIDTH_BOXES / nr_boxes;
+            else
+                new_width = _MAX_WIDTH_LAYER_BOX;
+
+            if (new_width > _MAX_WIDTH_LAYER_BOX)
+                new_width = _MAX_WIDTH_LAYER_BOX;
+
+            adjust_boxes (new_width, layer);
+            box_width = new_width;
+        }
+        break;
+
+    case LCO_JOIN_CLIENT:
+        break;
+    case LCO_REMOVE_CLIENT:
+        break;
+    case LCO_TOPMOST_CHANGED:
+        on_change_topmost (layer);
+        break;
+    case LCO_ACTIVE_CHANGED:
+        break;
+    default:
+        printf ("Serious error: incorrect operations.\n");
+    }
+}
+
+static void child_wait (int sig)
+{
+    int pid;
+    int status;
+
+    while ((pid = waitpid (-1, &status, WNOHANG)) > 0) {
+        if (WIFEXITED (status))
+            printf ("--pid=%d--status=%x--rc=%d---\n", 
+                            pid, status, WEXITSTATUS(status));
+        else if (WIFSIGNALED(status))
+            printf ("--pid=%d--signal=%d--\n", pid, WTERMSIG (status));
+    }
+}
+
+int MiniGUIMain (int args, const char* arg[])
+{
+    struct sigaction siga;
+    MSG msg;
+
+    siga.sa_handler = child_wait;
+    siga.sa_flags  = 0;
+    memset (&siga.sa_mask, 0, sizeof(sigset_t));
+    sigaction (SIGCHLD, &siga, NULL);
+
+    OnNewDelClient = on_new_del_client;
+    OnChangeLayer = on_change_layer;
+
+    if (!ServerStartup (0, 0, 0)) {
+        fprintf (stderr, 
+                 "Can not start the server of MiniGUI-Processes: mginit.\n");
+        return 1;
+    }
+
+    if ((hStatusBar = create_task_bar ()) == HWND_INVALID) {
+        fprintf (stderr, "Can not create task bar.\n");
+        return 2;
+    }
+
+    while (GetMessage (&msg, hStatusBar)) {
+        DispatchMessage (&msg);
+    }
+
+    return 0;
+}
+
+#endif
+
---
+++ device-side/hishell/mginit/statusbar.c
@@ -0,0 +1,347 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//                          IMPORTANT NOTICE
+//
+// The following open source license statement does not apply to any
+// entity in the Exception List published by FMSoft.
+//
+// For more information, please visit:
+//
+// https://www.fmsoft.cn/exception-list
+//
+//////////////////////////////////////////////////////////////////////////////
+/* 
+** $Id: taskbar.c 381 2008-01-28 10:19:26Z wangjian $
+**
+** The taskbar of MDE
+**
+** Copyright (C) 2003 ~ 2017 FMSoft (http://www.fmsoft.cn).
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <minigui/common.h>
+#include <minigui/minigui.h>
+#include <minigui/gdi.h>
+#include <minigui/window.h>
+#include <minigui/control.h>
+
+#include "statusbar.h"
+
+static BOOL quit = FALSE;
+
+static char* mk_time (char* buff)
+{
+    time_t t;
+    struct tm * tm;
+
+    time (&t);
+    tm = localtime (&t);
+    sprintf (buff, "%02d:%02d:%02d", tm->tm_hour, tm->tm_min, tm->tm_sec);
+
+    return buff;
+}
+
+APPINFO app_info;
+
+static void free_app_info (void)
+{
+    int i;
+    APPITEM* item;
+
+    item = app_info.app_items;
+    for (i = 0; i < app_info.nr_apps; i++, item++) {
+        if (item->bmp.bmBits) {
+            UnloadBitmap (&item->bmp);
+            item->bmp.bmBits = NULL;
+        }
+    }
+
+    free (app_info.app_items);
+    app_info.app_items = NULL;
+}
+
+static void strsubchr (char* string, int c1, int c2)
+{
+    char* tmp;
+
+    while (string && (tmp = strchr (string, c1))) {
+        *tmp = c2;
+        string = tmp;
+    }
+}
+
+static BOOL get_app_info (void)
+{
+    int i;
+    APPITEM* item;
+    char section [32];
+
+    if (GetIntValueFromEtcFile (APP_INFO_FILE, "taskbar", "nr", &app_info.nr_apps) != ETC_OK)
+        return FALSE;
+
+    if (app_info.nr_apps <= 0)
+        return FALSE;
+
+    GetIntValueFromEtcFile (APP_INFO_FILE, "taskbar", "autostart", &app_info.autostart);
+    
+    if (GetValueFromEtcFile (APP_INFO_FILE, "taskbar", "logo", app_info.logo_path, PATH_MAX + NAME_MAX) != ETC_OK)
+        return FALSE;
+           
+
+    if (app_info.autostart >= app_info.nr_apps || app_info.autostart < 0)
+        app_info.autostart = 0;
+
+    if ((app_info.app_items = (APPITEM*)calloc (app_info.nr_apps, sizeof (APPITEM))) == NULL) {
+        return FALSE;
+    }
+
+    item = app_info.app_items;
+    for (i = 0; i < app_info.nr_apps; i++, item++) {
+
+        sprintf (section, "app%d", i);
+        if (GetValueFromEtcFile (APP_INFO_FILE, section, "path", item->path, PATH_MAX) != ETC_OK)
+            goto error;
+
+        if (GetValueFromEtcFile (APP_INFO_FILE, section, "name", item->name, NAME_MAX) != ETC_OK)
+            goto error;
+
+        if (GetValueFromEtcFile (APP_INFO_FILE, section, "layer", item->layer, LEN_LAYER_NAME) != ETC_OK)
+            goto error;
+
+        if (GetValueFromEtcFile (APP_INFO_FILE, section, "tip", item->tip, TIP_MAX) != ETC_OK)
+            goto error;
+
+        strsubchr (item->tip, '&', ' ');
+
+        if (GetValueFromEtcFile (APP_INFO_FILE, section, "icon", item->bmp_path, PATH_MAX + NAME_MAX) != ETC_OK)
+            goto error;
+
+        if (LoadBitmap (HDC_SCREEN, &item->bmp, item->bmp_path) != ERR_BMP_OK)
+            goto error;
+
+        item->cdpath = TRUE;
+    }
+    return TRUE;
+
+error:
+    free_app_info ();
+    return FALSE;
+}
+
+static HWND create_app_coolbar (HWND hWnd)
+{
+    int i;
+    HWND hCoolBar;
+    COOLBARITEMINFO bar_item;
+    APPITEM* app_item;
+
+    hCoolBar = CreateWindow (CTRL_COOLBAR, "",
+                        WS_CHILD | WS_VISIBLE, _ID_APPS_COOLBAR,
+                        _WIDTH_START + _MARGIN * 2, _MARGIN,
+                        _WIDTH_APPS, _HEIGHT_CTRL,
+                        hWnd, 0);
+
+    bar_item.insPos = 0;
+    bar_item.id = 0;
+    bar_item.ItemType = TYPE_TEXTITEM;
+    bar_item.Bmp = NULL;
+    bar_item.ItemHint = "Quit MDE";
+    bar_item.Caption = "Quit";
+    bar_item.dwAddData= 0;
+
+    SendMessage (hCoolBar, CBM_ADDITEM, 0, (LPARAM)&bar_item);
+
+    app_item = app_info.app_items;
+    for (i = 0; i < app_info.nr_apps; i++, app_item++) {
+        bar_item.insPos = i + 1;
+        bar_item.id = i + 1;
+        bar_item.ItemType = TYPE_BMPITEM;
+        bar_item.Bmp = &app_item->bmp;
+        bar_item.ItemHint = app_item->tip;
+        bar_item.Caption = NULL;
+        bar_item.dwAddData= 0;
+
+        SendMessage (hCoolBar, CBM_ADDITEM, 0, (LPARAM)&bar_item);
+    }
+
+    return hCoolBar;
+}
+
+static void under_construction (HWND hwnd)
+{
+    MessageBox (hwnd, 
+            "This function is under construction.",
+            "MDE!",
+            MB_OK | MB_ICONEXCLAMATION);
+}
+
+static void ask_for_quit (HWND hwnd)
+{
+    if (MessageBox (hwnd, 
+            "You are asking for quit. \n\nDo you want to quit really?",
+            "Do you want to quit really?",
+            MB_YESNO | MB_ICONQUESTION) == IDYES) {
+        quit = TRUE;
+    }
+}
+
+static BITMAP * plogo = NULL;
+static LRESULT TaskBarWinProc (HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    char buff [20];
+
+    switch (message) {
+    case MSG_CREATE:
+    {
+        if (!get_app_info ())
+            return 1;
+        plogo = calloc (1 , sizeof(BITMAP));
+        LoadBitmap (HDC_SCREEN , plogo , app_info.logo_path);
+/*
+        CreateWindow (CTRL_BUTTON, "Start", WS_CHILD | WS_VISIBLE, _ID_START_BUTTON, 
+                    _MARGIN, _MARGIN, _WIDTH_START, _HEIGHT_CTRL, hWnd, 0);
+                    */
+        CreateWindow (CTRL_STATIC, "", SS_REALSIZEIMAGE | SS_CENTERIMAGE | SS_BITMAP | WS_CHILD | WS_VISIBLE, _ID_START_BUTTON, 
+                    _MARGIN, _MARGIN, _WIDTH_START, _HEIGHT_CTRL, hWnd, (DWORD)plogo);
+
+        CreateWindow (CTRL_STATIC, mk_time (buff), WS_CHILD | WS_BORDER | WS_VISIBLE | SS_CENTER, 
+                    _ID_TIME_STATIC, g_rcScr.right - _WIDTH_TIME - _MARGIN, _MARGIN, 
+                    _WIDTH_TIME, _HEIGHT_CTRL, hWnd, 0);
+
+        create_app_coolbar (hWnd);
+
+#ifdef _MGTIMER_UNIT_10MS
+        SetTimer (hWnd, _ID_TIMER, 100);
+#else
+        SetTimer (hWnd, _ID_TIMER, 10);
+#endif
+        break;
+    }
+
+    case MSG_COMMAND:
+    {
+        int code = HIWORD (wParam);
+        int id   = LOWORD (wParam);
+        switch (id) {
+        case _ID_START_BUTTON:
+            under_construction (hWnd);
+            break;
+        case _ID_APPS_COOLBAR:
+            if (code == 0) {
+                ask_for_quit (hWnd);
+            }
+            else
+                exec_app (code - 1);
+            break;
+        }
+
+        if (id == _ID_LAYER_BOX && code == 0) {
+            MG_Layer* layer = (MG_Layer*) GetWindowAdditionalData ((HWND)lParam);
+
+            if (layer) {
+                ServerSetTopmostLayer (layer);
+            }
+        }
+    }
+    break;
+
+    case MSG_TIMER:
+    {
+        SetDlgItemText (hWnd, _ID_TIME_STATIC, mk_time (buff));
+        if (quit)
+            PostMessage (hWnd, MSG_CLOSE, 0, 0);
+        break;
+    }
+        
+    case MSG_CLOSE:
+        KillTimer (hWnd, _ID_TIMER);
+        free (plogo);
+        DestroyAllControls (hWnd);
+        free_app_info ();
+        DestroyMainWindow (hWnd);
+        PostQuitMessage (hWnd);
+        return 0;
+    }
+
+    return DefaultMainWinProc (hWnd, message, wParam, lParam);
+}
+
+HWND create_task_bar (void)
+{
+    MAINWINCREATE CreateInfo;
+    HWND hTaskBar;
+
+    CreateInfo.dwStyle = WS_ABSSCRPOS | WS_VISIBLE;
+    CreateInfo.dwExStyle = WS_EX_TOOLWINDOW;
+    CreateInfo.spCaption = "TaskBar" ;
+    CreateInfo.hMenu = 0;
+    CreateInfo.hCursor = GetSystemCursor (0);
+    CreateInfo.hIcon = 0;
+    CreateInfo.MainWindowProc = TaskBarWinProc;
+    CreateInfo.lx = g_rcScr.left; 
+    CreateInfo.ty = 0;  // g_rcScr.bottom - HEIGHT_TASKBAR;
+    CreateInfo.rx = g_rcScr.right;
+    CreateInfo.by = HEIGHT_TASKBAR; // g_rcScr.bottom;
+    CreateInfo.iBkColor =
+        GetWindowElementPixelEx (HWND_NULL, HDC_SCREEN, WE_MAINC_THREED_BODY); 
+    CreateInfo.dwAddData = 0;
+    CreateInfo.hHosting = HWND_DESKTOP;
+
+    hTaskBar = CreateMainWindow (&CreateInfo);
+
+    return hTaskBar;
+}
+
+pid_t exec_app (int app)
+{
+    pid_t pid = 0;
+    char buff [PATH_MAX + NAME_MAX + 1];
+
+    if ((pid = vfork ()) > 0) {
+        fprintf (stderr, "new child, pid: %d.\n", pid);
+    }
+    else if (pid == 0) {
+        if (app_info.app_items [app].cdpath) {
+            if (chdir (app_info.app_items [app].path)) {
+                fprintf (stderr, "error on chdir.\n");
+                return 0;
+            }
+        }
+        strcpy (buff, app_info.app_items [app].path);
+        strcat (buff, app_info.app_items [app].name);
+
+        if (app_info.app_items [app].layer [0]) {
+            execl (buff, app_info.app_items [app].name, 
+                        "-layer", app_info.app_items [app].layer, NULL);
+        }
+        else {
+            execl (buff, app_info.app_items [app].name, NULL);
+        }
+
+        perror ("execl");
+        _exit (1);
+    }
+    else {
+        perror ("vfork");
+    }
+
+    return pid;
+}
+
